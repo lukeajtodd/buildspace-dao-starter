0d6f6860366568bfae0f8b8e68bb913e2b57241e
Luke Todd <luketodd@zoho.com>
2021-12-19 17:23:16 +0000

    Init and allow connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-const App = () => {
-  return (
-    <div className="landing">
-      <h1>Welcome to My DAO</h1>
-    </div>
-  );
-};
+import { useEffect, useMemo, useState } from "react";
+import { useWeb3 } from "@3rdweb/hooks";
 
-export default App;
+const App = () => {
+    const { connectWallet, address, provider, error } = useWeb3()
+    console.log("Address: ", address)
+
+    if (!address) {
+      return (
+        <div className="loading">
+          <h1>Welcome to GenericDAO</h1>
+          <button onClick={() => connectWallet("injected")} className="btn-hero">
+            Connect
+          </button>
+        </div>
+      )
+    }
+
+    return (
+      <div className="landing">
+        <h1>We have a wallet!</h1>
+      </div>
+    )
+}
+
+export default App
